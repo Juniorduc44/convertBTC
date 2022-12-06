7a172530a116bedf64a8d02155369c3bbc945de7
Juniorduc44 <Juniorduc44@gmail.com>
2022-12-05 22:06:49 -0800

    v0.1.1

--- bitcoinConverter.py
+++ bitcoinConverter.py
@@ -1,14 +1,10 @@
+__version__="0.1.1"
 import tkinter
 import customtkinter
 from blockchain import exchangerates as ex
 
 
-#Bitcoin Price Tracker
-ticker = ex.get_ticker()
-for k in ticker:
-    if k == "USD":
-        btcPrice = (ticker[k].p15min)
-        
+
 
 customtkinter.set_appearance_mode("System") #Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue") # Themes: blue (default), dark-blue, green
@@ -22,6 +18,11 @@ frame1 = customtkinter.CTkFrame(master = app) #master or placement is the "app" 
 frame1.pack(pady=20, padx=60, fill="both", expand=True)
 
 
+#Bitcoin Price Tracker
+ticker = ex.get_ticker()
+for k in ticker:
+    if k == "USD":
+        btcPrice = (ticker[k].p15min)
 
 #Bitcoin Label for the current interface showing
 label1 = customtkinter.CTkLabel(master=frame1, justify=tkinter.LEFT,text=f'''
@@ -30,11 +31,11 @@ ${btcPrice}''')
 label1.pack(pady=10, padx=10)
 
 
+#Function that takes the dollar entry input to convert to bitcoin
 def button_function():
     entryState = entry.get()
     e = entryState
-    if e.isdigit():
-        
+    if e.isdigit():        
         btcAmount = int(e)/btcPrice
         btcA = ("{:.8f}".format(btcAmount))
         print(f"{btcA} BTC")
@@ -49,11 +50,17 @@ def button_function():
 entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount")
 entry.pack(padx=20, pady=10)
 
+#Dropdown menu Next to entry box for currency conversion
+#???Need to make this drop menu smaller and adjacent to the left of the entry box. Default USD.
+dropMenu1 = customtkinter.CTkOptionMenu(frame1, values=["USD", "CAD", "JPY", "GBP"])
+dropMenu1.pack(padx=10, pady=10)
+dropMenu1.set("Fiat Type")
 
 #Use CTKButton instead of tkinter Button
 #used pack to keep button in place instead of using place which lets it move with window
 button = customtkinter.CTkButton(master=frame1, text="Convert", command=button_function)
 button.pack(padx=25, pady=10)
+
 
 
 
