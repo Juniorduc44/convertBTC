5fdcdb6aa4fa631e7be4d2b32836836ecc1eac83
Juniorduc44 <Juniorduc44@gmail.com>
2022-12-10 23:35:47 -0800

    v0.1.5

--- bitcoinConverter.py
+++ bitcoinConverter.py
@@ -1,4 +1,4 @@
-__version__="0.1.4"
+__version__="0.1.5"
 import tkinter
 import customtkinter
 from blockchain import exchangerates as ex
@@ -18,16 +18,15 @@ def button_function():
     else:
         print(False)
 
-customtkinter.set_appearance_mode("System") #Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue") # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("System").set_default_color_theme("blue") #Modes: system (default), light, dark
+ # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk() #create CTK window like you do with the Tk window
 app.geometry("400x300")
 app.title("Bitcoin Converter App")
 
 #creates a frame effect in background
-frame1 = customtkinter.CTkFrame(master = app) #master or placement is the "app" defined
-frame1.pack(pady=10, padx=15, expand=False)
+frame1 = customtkinter.CTkFrame(master = app).pack(pady=10, padx=15, expand=False) #master or placement is the "app" defined
 
 #Bitcoin Price Tracker
 ticker = ex.get_ticker()
@@ -42,31 +41,26 @@ ${btcPrice}''')
 label1.pack(pady=10, padx=10)
 
 #Entry box for the conversion
-entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount")
-entry.pack(padx=20, pady=10)
+entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount").pack(padx=25, pady=10)
 
 #Dropdown menu Next to entry box for currency conversion
 #???Need to make this drop menu smaller and adjacent to the left of the entry box. Default USD.
-dropMenu1 = customtkinter.CTkOptionMenu(frame1, values=["USD", "CAD", "JPY", "GBP"])
-dropMenu1.pack(padx=10, pady=10)
-dropMenu1.set("Fiat Type")
+#dropMenu1 = customtkinter.CTkOptionMenu(frame1, values=["USD", "CAD", "JPY", "GBP"])
+#dropMenu1.pack(padx=10, pady=10)
+#dropMenu1.set("Fiat Type")
 
 #Use CTKButton instead of tkinter Button
 #used pack to keep button in place instead of using place which lets it move with window
-button = customtkinter.CTkButton(master=frame1, text="Convert", command=button_function)
-button.pack(padx=25, pady=10)
+button = customtkinter.CTkButton(master=frame1, text="Convert", command=button_function).pack(padx=25, pady=10)
 
 #Function to delete last result
 def myDelete():
     label.configure(text="")
 
 #button to delete the label of result
-dbutton = customtkinter.CTkButton(master=frame1, text="Delete", command=myDelete)
-dbutton.pack(padx=25, pady=1)
+dbutton = customtkinter.CTkButton(master=frame1, text="Delete", command=myDelete).pack(padx=25, pady=1)
 
 #widget to create placement for the results
-label = customtkinter.CTkLabel(master=frame1, text="")
-label.pack()
-
+label = customtkinter.CTkLabel(master=frame1, text="").pack()
 
 app.mainloop()
