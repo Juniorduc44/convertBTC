e4f960798e6d999845268a23471cfcf3d1abd5cf
Juniorduc44 <Juniorduc44@gmail.com>
2022-12-18 19:53:01 -0800

    v0.1.6

--- bitcoinConverter.py
+++ bitcoinConverter.py
@@ -1,7 +1,12 @@
-__version__="0.1.5"
+__version__="0.1.6"
 import tkinter
 import customtkinter
 from blockchain import exchangerates as ex
+
+
+#Function to delete last result
+def myDelete():
+    label.configure(text="")
 
 
 #Function that takes the dollar entry input to convert to bitcoin
@@ -17,6 +22,8 @@ def button_function():
         label.configure(text=f"Results: {btcA} BTC")
     else:
         print(False)
+
+
 
 customtkinter.set_appearance_mode("System")#.set_default_color_theme("blue") #Modes: system (default), light, dark
  # Themes: blue (default), dark-blue, green
@@ -42,7 +49,8 @@ ${btcPrice}''')
 label1.pack(pady=10, padx=10)
 
 #Entry box for the conversion
-entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount").pack(padx=25, pady=10)
+entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount")
+entry.pack(padx=25, pady=10)
 
 #Dropdown menu Next to entry box for currency conversion
 #???Need to make this drop menu smaller and adjacent to the left of the entry box. Default USD.
@@ -54,14 +62,12 @@ entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Enter $ Amount")
 #used pack to keep button in place instead of using place which lets it move with window
 button = customtkinter.CTkButton(master=frame1, text="Convert", command=button_function).pack(padx=25, pady=10)
 
-#Function to delete last result
-def myDelete():
-    label.configure(text="")
 
 #button to delete the label of result
 dbutton = customtkinter.CTkButton(master=frame1, text="Delete", command=myDelete).pack(padx=25, pady=1)
 
 #widget to create placement for the results
-label = customtkinter.CTkLabel(master=frame1, text="").pack()
+label = customtkinter.CTkLabel(master=frame1, text="")
+label.pack()
 
 app.mainloop()
